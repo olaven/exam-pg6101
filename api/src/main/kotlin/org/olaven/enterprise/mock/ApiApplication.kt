29e17fc62cf305fa33931b460d8c29529d9de5bf
olaven <olav@sundfoer.com>
2019-11-03 15:14:03 +0100

    added swagger documentation

--- api/src/main/kotlin/org/olaven/enterprise/mock/ApiApplication.kt
+++ api/src/main/kotlin/org/olaven/enterprise/mock/ApiApplication.kt
@@ -2,12 +2,25 @@ package org.olaven.enterprise.mock
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.context.annotation.Bean
 import springfox.documentation.swagger2.annotations.EnableSwagger2
+import springfox.documentation.builders.PathSelectors
+import springfox.documentation.spi.DocumentationType
+import springfox.documentation.spring.web.plugins.Docket
 
 @SpringBootApplication
 //@EnableEurekaClient //TODO
 @EnableSwagger2
-class ApiApplication
+class ApiApplication {
+
+    @Bean
+    fun swaggerApi(): Docket {
+        return Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .paths(PathSelectors.any())
+                .build()
+    }
+}
 
 fun main(args: Array<String>) {
     SpringApplication.run(ApiApplication::class.java, *args)
