25c5589d082391af0d9e9465b73a1a9aa6db7e3b
olaven <olav@sundfoer.com>
2019-11-04 17:06:54 +0100

    everything ocmpiles and runs, but MOVIES crashes

--- movies/src/test/kotlin/org/olaven/enterprise/mock/movies/LocalApplicationRunner.kt
+++ movies/src/test/kotlin/org/olaven/enterprise/mock/movies/LocalApplicationRunner.kt
@@ -3,7 +3,6 @@ package org.olaven.enterprise.mock.movies
 import org.springframework.boot.SpringApplication
 
 // NOTE: will use h2, as application.yml in _test_ is used
-// requires Postgres on "postgres" domain name
 fun main(args: Array<String>) {
     SpringApplication.run(MoviesApplication::class.java, *args)
 }
