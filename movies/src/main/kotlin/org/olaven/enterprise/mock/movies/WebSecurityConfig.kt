fce6b6d6ee0f44d6b957de5da42084e79c05686f
olaven <olav@sundfoer.com>
2019-11-13 11:48:43 +0100

    fixed 404 test

--- movies/src/main/kotlin/org/olaven/enterprise/mock/movies/WebSecurityConfig.kt
+++ movies/src/main/kotlin/org/olaven/enterprise/mock/movies/WebSecurityConfig.kt
@@ -30,6 +30,7 @@ class WebSecurityConfig: WebSecurityConfigurerAdapter() {
                 .antMatchers(HttpMethod.GET, "/directors/{id}").permitAll()
                 //TODO/NOTE: below should perhaps only be admin-access, but I wanted to test this in e2e-tests, where I cannot reveal production-admins.
                 .antMatchers(HttpMethod.POST, "/directors").authenticated() //.access("hasRole('ADMIN')")
+                .antMatchers(HttpMethod.GET, "/doesNotExist").permitAll() //NOTE: added for the sake of testing 404. 
                 .anyRequest().denyAll() //enabling whitelist
                 .and()
                 .csrf().disable()
