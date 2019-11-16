534bfb27a3ac183a18e75778beb52821bd6d8edb
olaven <olav@sundfoer.com>
2019-11-16 22:00:59 +0100

    prepared seat-count-test before actually adding it to movies

--- graphql/src/main/kotlin/org/olaven/enterprise/mock/graphql/ScreeningsCaller.kt
+++ graphql/src/main/kotlin/org/olaven/enterprise/mock/graphql/ScreeningsCaller.kt
@@ -1,6 +1,7 @@
 package org.olaven.enterprise.mock.graphql
 
 import org.olaven.enterprise.mock.shared.WrappedResponse
+import org.olaven.enterprise.mock.shared.dto.ScreeningDTO
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 import org.springframework.web.client.RestTemplate
@@ -11,14 +12,12 @@ class ScreeningsCaller {
     @Value("\${external.api-base}")
     private var apiBase: String? = null
 
-    fun getAvailableSeats(id: Long): String? {
+    fun getAvailableSeats(id: Long): Int? {
 
-        val client =  RestTemplate()
+        val client =  RestTemplate()  //TODO: is this relevant for circuit breakers?
         val response = client.getForEntity("$apiBase/screenings/$id", WrappedResponse::class.java)
+        val screening = response.body.data as ScreeningDTO
 
-
-        println(response.body);
-        //return getFromJson(json!!, "data.availableSeats") //TODO: is this relevant for circuit breakers?
-        return "TODO"
+        return null; // change to screening.availableSeats
     }
 }
