50fc384ae629a9339b812578c1d2783c5ce93cc7
olaven <olav@sundfoer.com>
2019-11-17 17:44:38 +0100

    fixed failing migration

--- movies/src/main/resources/db/migration/V3__ticket_count.sql
+++ movies/src/main/resources/db/migration/V3__ticket_count.sql
@@ -1,8 +1,8 @@
-drop table if exists director_entity;
-drop table if exists director_entity_movies;
-drop table if exists movie_entity;
-drop table if exists screening_entity;
-drop sequence if exists hibernate_sequence;
+drop table if exists director_entity cascade;
+drop table if exists director_entity_movies cascade;
+drop table if exists movie_entity cascade;
+drop table if exists screening_entity cascade;
+drop sequence if exists hibernate_sequence cascade;
 create sequence hibernate_sequence start with 1 increment by 1;
 create table director_entity (id bigint not null, family_name varchar(100), given_name varchar(100), primary key (id));
 create table director_entity_movies (director_entity_id bigint not null, movies_id bigint not null);
