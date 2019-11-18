37612fdfcff069c009fb0afef37183a25440b160
olaven <olav@sundfoer.com>
2019-11-18 17:51:03 +0100

    added default users

---
+++ authentication/src/main/kotlin/org/enterprise/exam/authentication/DefaultUserInitializer.kt
@@ -0,0 +1,21 @@
+package org.enterprise.exam.authentication
+
+import org.enterprise.exam.authentication.user.UserEntity
+import org.enterprise.exam.authentication.user.UserRepository
+import org.enterprise.exam.authentication.user.UserService
+import org.springframework.stereotype.Component
+import javax.annotation.PostConstruct
+
+@Component
+class DefaultUserInitializer(
+        private val userService: UserService
+) {
+
+    @PostConstruct
+    fun addDefaultUsers(){
+
+        userService.createUser("admin", "admin", setOf("ROLE_USER", "ROLE_ADMIN"))
+        userService.createUser("adam", "adampass", setOf("ROLE_USER"))
+        userService.createUser("charlie", "charliepass", setOf("ROLE_USER"))
+    }
+}
