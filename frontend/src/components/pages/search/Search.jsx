27b3cf372850a9906aa9c97e88f853d87aaf62b1
olaven <olav@sundfoer.com>
2019-11-20 20:29:48 +0100

    search has "load more"

--- frontend/src/components/pages/search/Search.jsx
+++ frontend/src/components/pages/search/Search.jsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import {Container, Header, Input} from "semantic-ui-react";
+import {Button, Container, Header, Input} from "semantic-ui-react";
 import {PaginationFetcher} from "../../../utils/PaginationFetcher";
 import {SearchResult} from "./SearchResult";
 
@@ -26,5 +26,6 @@ export const Search = () => {
       <Header as={"h2"}>Search for friends:</Header>
       <Input placeholder={"email"} onChange={(event) => {setSearchTerm(event.target.value)}}/>
       {userPage.list.map(user => <SearchResult user={user}/>)}
+      <Button onClick={() => {setLocation(userPage.next)}}>Load more</Button>
    </Container>;
 };
--- frontend/src/components/pages/search/SearchResult.jsx
+++ frontend/src/components/pages/search/SearchResult.jsx
@@ -3,7 +3,7 @@ import {Container, Header} from "semantic-ui-react";
 
 export const SearchResult = props => <Container>
 
-    <Header as={"h3"}>{props.user.givenName} {props.user.familyName}</Header>
+    <Header as={"h3"} style={{borderTop: "solid black"}}>{props.user.givenName} {props.user.familyName}</Header>
     <Header as={"h4"}>{props.user.email}</Header>
     TODO: PROFILE LINK
 </Container>;
